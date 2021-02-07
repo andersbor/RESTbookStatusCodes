e392353d78eafc29b34efab91c44200284a84cdc
Anders Børjesson <anbo@zealand.dk>
2021-02-07 11:09:16 +0100

    Add project files.

---
+++ RESTbookStatusCodes/Controllers/BooksController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using RESTbookStatusCodes.Managers;
+using RESTbookStatusCodes.Models;
+
+namespace RESTbookStatusCodes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksManager _manager = new BooksManager();
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IEnumerable<Book> Get([FromQuery] string title, [FromQuery] string sort_by)
+        {
+            return _manager.GetAll(title, sort_by);
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Book> Get(int id)
+        {
+            Book book = _manager.GetById(id);
+            if (book == null) return NotFound("No such book, id: " + id);
+            return Ok(book);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public ActionResult<Book> Post([FromBody] Book value)
+        {
+            Book newBook = _manager.Add(value);
+            string uri = Url.RouteUrl(RouteData.Values) + "/" + newBook.Id;
+            return Created(uri, newBook);
+
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Book> Put(int id, [FromBody] Book value)
+        {
+            Book updatedBook = _manager.Update(id, value);
+            if (updatedBook == null) return NotFound("No such book, id: " + id);
+            return Ok(updatedBook);
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Book> Delete(int id)
+        {
+            Book deletedBook = _manager.Delete(id);
+            if (deletedBook == null) return NotFound("No such book, id: " + id);
+            return Ok(deletedBook);
+        }
+    }
+}
---
+++ RESTbookStatusCodes/Managers/BooksManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RESTbookStatusCodes.Models;
+
+namespace RESTbookStatusCodes.Managers
+{
+    public class BooksManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<Book> Data = new List<Book>
+        {
+            new Book {Id = _nextId++, Title = "C# is nice", Price = 12.34},
+            new Book {Id=_nextId++, Title = "C# advanced", Price = 22.33},
+            new Book {Id = _nextId++, Title = "ABC for beginners", Price= 19.95}
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers
+        };
+
+        public List<Book> GetAll(string title = null, string sortBy = null)
+        // Optional parameters
+        // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/named-and-optional-arguments
+        {
+            List<Book> books = new List<Book>(Data);
+            // copy constructor
+            // Callers should no get a reference to the Data object, but rather get a copy
+
+            if (title != null)
+            {
+                books = books.FindAll(book => book.Title.StartsWith(title));
+            }
+            if (sortBy != null)
+            {
+                switch (sortBy.ToLower())
+                {
+                    case "title":
+                        books = books.OrderBy(book => book.Title).ToList();
+                        break;
+                    case "price":
+                        books = books.OrderBy(book => book.Price).ToList();
+                        break;
+                        // skip any other properties in the query string
+                }
+            }
+            return books;
+        }
+
+        public Book GetById(int id)
+        {
+            return Data.Find(book => book.Id == id);
+        }
+
+        public Book Add(Book newBook)
+        {
+            newBook.Id = _nextId++;
+            Data.Add(newBook);
+            return newBook;
+        }
+
+        public Book Delete(int id)
+        {
+            Book book = Data.Find(book1 => book1.Id == id);
+            if (book == null) return null;
+            Data.Remove(book);
+            return book;
+        }
+
+        public Book Update(int id, Book updates)
+        {
+            Book book = Data.Find(book1 => book1.Id == id);
+            if (book == null) return null;
+            book.Title = updates.Title;
+            book.Price = updates.Price;
+            return book;
+        }
+    }
+}
